b561b3c2004ee3540503dd3a95d47fedc9ab1bc8
Anton <anton.gayvoronskyy@googlemail.com>
2023-09-01 15:37:55 +0300

    added Hero block

---
+++ src/modules/slider.js
@@ -0,0 +1,35 @@
+let slider = () => {
+  const namesArr = document.querySelectorAll(".slider__img");
+  const numArr = document.querySelectorAll(".slider__nav-num");
+
+  let count = 0;
+
+  numArr.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      count = index;
+      changeSlide();
+    });
+  });
+
+  function changeSlide() {
+    addActiveClass(namesArr);
+    addActiveClass(numArr);
+  }
+
+  function addActiveClass(arr) {
+    for (let elem of arr) {
+      elem.classList.remove("active");
+    }
+    arr[count].classList.add("active");
+  }
+
+  document.addEventListener("DOMContentLoaded", () => {
+    setInterval(() => {
+      count++;
+      if (count > numArr.length - 1) count = 0;
+      changeSlide();
+    }, 5000);
+  });
+};
+
+export { slider };
